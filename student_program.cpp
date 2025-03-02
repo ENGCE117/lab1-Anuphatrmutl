caf002f10157f82d9b562707fbef4c2fff170c10
Anuphat <anuphatkk@gmail.com>
2025-03-02 19:03:38 +0700

    Update student_program.cpp

--- student_program.cpp
+++ student_program.cpp
@@ -1,6 +1,47 @@
-#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+void GetMatrix(int **&value, int &row, int &col);
+void PrintMatrix(int **value, int row, int col);
 
 int main() {
-    //YOUR CODE HERE
-    return 0 ;
-}//end function
+    int **data, m, n;
+
+    GetMatrix(data, m, n);
+    PrintMatrix(data, m, n);
+
+    for (int i = 0; i < m; i++) {
+        delete[] data[i];
+    }
+    delete[] data;
+
+    return 0;
+}
+
+void GetMatrix(int **&value, int &row, int &col) {
+    cout << "Enter rows and columns: ";
+    cin >> row >> col;
+
+    value = new int *[row];
+    for (int i = 0; i < row; i++) {
+        value[i] = new int[col];
+    }
+
+    cout << "Enter matrix values:\n";
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            cin >> value[i][j];
+        }
+    }
+}
+
+void PrintMatrix(int **value, int row, int col) {
+    cout << "Matrix (" << row << "x" << col << "):\n";
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            cout << value[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
